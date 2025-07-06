379fba4588708b868f623661dcde619c86015476
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-07-06 22:05:58 +0300

    telemetry-node: add GrpcClientRetrySendInterval to config and .env

--- telemetry-node/internal/config/config.go
+++ telemetry-node/internal/config/config.go
@@ -8,20 +8,22 @@ import (
 
 // Config represents app config
 type Config struct {
-	SensorName                       string
-	SensorInterval                   time.Duration
-	GrpcClientMessageBatchPerRequest int64
-	GrpcClientMaxRetryAttempts       int64
-	GrpcServer                       string
+	SensorName                  string
+	SensorInterval              time.Duration
+	MessageBatchPerRequest      int64
+	GrpcClientMaxRetryAttempts  int64
+	GrpcClientRetrySendInterval time.Duration
+	GrpcServer                  string
 }
 
 // LoadConfigEnv sets Config with environment variables values
 func LoadConfigEnv() Config {
 	return Config{
-		SensorName:                       env.EnvironmentVariable("SENSOR_NAME", "default-sensor-name"),
-		SensorInterval:                   env.ParseDurationEnv("SENSOR_INTERVAL", time.Second),
-		GrpcClientMessageBatchPerRequest: env.ParseIntEnv("GRPC_CLIENT_MESSAGE_BATCH_PER_REQUEST", 10),
-		GrpcClientMaxRetryAttempts:       env.ParseIntEnv("GRPC_CLIENT_MAX_RETRY_ATTEMPT_NUMBER", 5),
-		GrpcServer:                       env.EnvironmentVariable("GRPC_SERVER", "localhost:9000"),
+		SensorName:                  env.EnvironmentVariable("SENSOR_NAME", "default-sensor-name"),
+		SensorInterval:              env.ParseDurationEnv("SENSOR_INTERVAL", time.Second),
+		MessageBatchPerRequest:      env.ParseIntEnv("MESSAGE_BATCH_PER_REQUEST", 1),
+		GrpcClientMaxRetryAttempts:  env.ParseIntEnv("GRPC_CLIENT_MAX_RETRY_ATTEMPTS_NUMBER", 5),
+		GrpcClientRetrySendInterval: env.ParseDurationEnv("GRPC_CLIENT_RETRY_SEND_INTERVAL", time.Second),
+		GrpcServer:                  env.EnvironmentVariable("GRPC_SERVER", "localhost:9000"),
 	}
 }

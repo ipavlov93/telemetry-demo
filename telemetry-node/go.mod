57360c47cfd42141b4d4501a7219c76421360eae
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-07-09 12:18:02 +0300

    telemetry-node: 1. add SensorService 2. go mod tidy

--- telemetry-node/go.mod
+++ telemetry-node/go.mod
@@ -2,10 +2,23 @@ module github.com/ipavlov93/telemetry-demo/telemetry-node
 
 go 1.24.4
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.3.2
+	github.com/ipavlov93/telemetry-demo/pkg v0.0.0-20250707201146-86dc28efc9d0
+	github.com/stretchr/testify v1.10.0
+	go.uber.org/zap v1.27.0
+	golang.org/x/time v0.12.0
+	google.golang.org/grpc v1.73.0
+	google.golang.org/protobuf v1.36.6
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ telemetry-node/internal/service/service.go
@@ -0,0 +1,87 @@
+package service
+
+import (
+	"context"
+	"strconv"
+
+	sensorpb "github.com/ipavlov93/telemetry-demo/pkg/grpc/generated/v1/sensor"
+	sensorapi "github.com/ipavlov93/telemetry-demo/pkg/grpc/generated/v1/sensor_service"
+	"github.com/ipavlov93/telemetry-demo/pkg/logger"
+	"github.com/ipavlov93/telemetry-demo/telemetry-node/internal/domain"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+// SensorService represents component that is responsible for delivery to destination server.
+type SensorService struct {
+	sensorClient sensorapi.SensorServiceClient
+	logger       logger.Logger
+}
+
+// NewSensorService returns pointer to created instance of SensorService.
+// It skips logger validation.
+func NewSensorService(
+	sensorClient sensorapi.SensorServiceClient,
+	lg logger.Logger,
+) *SensorService {
+	return &SensorService{
+		sensorClient: sensorClient,
+		logger:       lg,
+	}
+}
+
+// SendSensorValues consumes sensor values (single slice) from the channel and sends them using corresponding client.
+// Notice: it won't drain remaining messages from channel if context is cancelled.
+func (s *SensorService) SendSensorValues(ctx context.Context, input <-chan []domain.SensorValue) {
+	for {
+		select {
+		case <-ctx.Done():
+			s.logger.Info("SensorService context cancelled")
+			return
+		case sensorValues, ok := <-input:
+			if !ok {
+				s.logger.Info("SensorService input channel closed")
+				return
+			}
+
+			// prepare request
+			req := toProtoRequest(sensorValues)
+
+			// send attempt with retry
+			// there won't be any retry attempts if server is totally unavailable due network issues
+			_, err := s.sensorClient.SendSensorValues(ctx, req)
+			if err != nil {
+				st := status.Convert(err)
+				s.logger.Debug("SensorServiceClient.SendSensorValues failed",
+					zap.String(strconv.Itoa(int(status.Code(err))), st.Message()))
+
+				continue
+			}
+			s.logger.Debug("SensorServiceClient.SendSensorValues successful",
+				zap.Int("messageCount", len(sensorValues)),
+			)
+		}
+	}
+}
+
+// toProtoRequest utility function set []domain.SensorValue to *sensorpb.SensorValuesRequest.
+func toProtoRequest(sensorValues []domain.SensorValue) *sensorpb.SensorValuesRequest {
+	valueBatches := make([]*sensorpb.SensorValue, len(sensorValues))
+	for i, v := range sensorValues {
+		valueBatches[i] = toProtoMessage(v)
+	}
+	return &sensorpb.SensorValuesRequest{Items: valueBatches}
+}
+
+// toProtoMessage utility function convert domain.SensorValue entity to *sensorpb.SensorValue DTO.
+func toProtoMessage(sensorValue domain.SensorValue) *sensorpb.SensorValue {
+	return &sensorpb.SensorValue{
+		SensorName: sensorValue.SensorName,
+		Measurement: &sensorpb.SensorValue_Measurement{
+			SensorValue: &wrapperspb.Int64Value{Value: sensorValue.Value},
+			CreatedAt:   timestamppb.New(sensorValue.Timestamp),
+		},
+	}
+}

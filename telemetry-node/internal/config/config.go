debfc5e3f73e509af9da81568c314a8d4196e0e5
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-07-09 12:17:05 +0300

    telemetry-node: update config and .env.example

--- telemetry-node/internal/config/config.go
+++ telemetry-node/internal/config/config.go
@@ -1,29 +1,27 @@
 package config
 
 import (
-	"time"
-
 	"github.com/ipavlov93/telemetry-demo/pkg/env"
 )
 
 // Config represents app config
 type Config struct {
-	SensorName                  string
-	SensorInterval              time.Duration
-	MessageBatchPerRequest      int64
-	GrpcClientMaxRetryAttempts  int64
-	GrpcClientRetrySendInterval time.Duration
-	GrpcServer                  string
+	SensorName string
+	// Number of requests per second to send
+	RequestRatePerSecond float32
+	// telemetry-sink gRPC socket
+	GrpcServerSocket           string
+	GrpcClientMaxRetryAttempts uint
+	LoggerMinLogLevel          string
 }
 
 // LoadConfigEnv sets Config with environment variables values
 func LoadConfigEnv() Config {
 	return Config{
-		SensorName:                  env.EnvironmentVariable("SENSOR_NAME", "default-sensor-name"),
-		SensorInterval:              env.ParseDurationEnv("SENSOR_INTERVAL", time.Second),
-		MessageBatchPerRequest:      env.ParseIntEnv("MESSAGE_BATCH_PER_REQUEST", 1),
-		GrpcClientMaxRetryAttempts:  env.ParseIntEnv("GRPC_CLIENT_MAX_RETRY_ATTEMPTS_NUMBER", 5),
-		GrpcClientRetrySendInterval: env.ParseDurationEnv("GRPC_CLIENT_RETRY_SEND_INTERVAL", time.Second),
-		GrpcServer:                  env.EnvironmentVariable("GRPC_SERVER", "localhost:9000"),
+		SensorName:                 env.EnvironmentVariable("SENSOR_NAME", "default-sensor-name"),
+		RequestRatePerSecond:       env.ParseFloat32Env("REQUEST_RATE_PER_SECOND", 1),
+		GrpcServerSocket:           env.EnvironmentVariable("GRPC_SERVER_SOCKET", "localhost:8000"),
+		GrpcClientMaxRetryAttempts: env.ParseUintEnv("GRPC_CLIENT_MAX_RETRY_ATTEMPTS_NUMBER", 5),
+		LoggerMinLogLevel:          env.EnvironmentVariable("LOGGER_MIN_LOG_LEVEL", "info"),
 	}
 }

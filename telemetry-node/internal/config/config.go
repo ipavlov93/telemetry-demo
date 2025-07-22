0ceae0cfebe9a4eaa3e9572c6a2723949e49f82e
Ivan Pavlov <i.pavlov.nix@gmail.com>
2025-07-22 13:31:41 +0300

    telemetry-node: 1. rename Config to AppConfig 2. add SensorValueRatePerSecond env variable

--- telemetry-node/internal/config/config.go
+++ telemetry-node/internal/config/config.go
@@ -1,27 +1,26 @@
 package config
 
-import (
-	"github.com/ipavlov93/telemetry-demo/pkg/env"
-)
+import "github.com/ipavlov93/telemetry-demo/pkg/env"
 
-// Config represents app config
-type Config struct {
+type AppConfig struct {
 	SensorName string
 	// Number of requests per second to send
-	RequestRatePerSecond float32
+	RequestRatePerSecond     float32
+	SensorValueRatePerSecond float32
 	// telemetry-sink gRPC socket
 	GrpcServerSocket           string
 	GrpcClientMaxRetryAttempts uint
 	LoggerMinLogLevel          string
 }
 
-// LoadConfigEnv sets Config with environment variables values
-func LoadConfigEnv() Config {
-	return Config{
+// LoadConfigEnv parses environment variables or set default values.
+func LoadConfigEnv() AppConfig {
+	return AppConfig{
 		SensorName:                 env.EnvironmentVariable("SENSOR_NAME", "default-sensor-name"),
+		SensorValueRatePerSecond:   env.ParseFloat32Env("SENSOR_VALUE_RATE_PER_SECOND", 5),
 		RequestRatePerSecond:       env.ParseFloat32Env("REQUEST_RATE_PER_SECOND", 1),
 		GrpcServerSocket:           env.EnvironmentVariable("GRPC_SERVER_SOCKET", "localhost:8000"),
-		GrpcClientMaxRetryAttempts: env.ParseUintEnv("GRPC_CLIENT_MAX_RETRY_ATTEMPTS_NUMBER", 5),
+		GrpcClientMaxRetryAttempts: env.ParseUintEnv("GRPC_CLIENT_MAX_RETRY_ATTEMPTS_NUMBER", 1),
 		LoggerMinLogLevel:          env.EnvironmentVariable("LOGGER_MIN_LOG_LEVEL", "info"),
 	}
 }

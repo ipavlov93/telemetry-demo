8b5a06a6387e27e34a000f6fbb2d9af67185b6c5
Ivan Pavlov <i.pavlov.nix@gmail.com>
2025-08-05 15:23:15 +0300

    telemetry-node: add Configuration instead of LogOutput to logger/factory/writer

--- telemetry-node/internal/infra/logger/factory/writer/writer.go
+++ telemetry-node/internal/infra/logger/factory/writer/writer.go
@@ -8,20 +8,20 @@ import (
 	config "github.com/ipavlov93/telemetry-demo/telemetry-node/internal/config/logger"
 )
 
-type Factory func(config.LogOutput) (io.Writer, error)
+type Factory func(config.Configuration) (io.Writer, error)
 
-func NewLogWriter(dest config.LogOutput) (io.Writer, error) {
-	switch dest.Type {
+func NewLogWriter(cfg config.Configuration) (io.Writer, error) {
+	switch cfg.Destination {
 	case config.LogOutputStdout:
 		return os.Stdout, nil
 
 	case config.LogOutputFile:
-		if dest.File == nil {
+		if cfg.File == nil {
 			return nil, fmt.Errorf("file config missing for file output")
 		}
-		return createFileWriter(*dest.File)
+		return createFileWriter(*cfg.File)
 
 	default:
-		return nil, fmt.Errorf("unsupported log output type: %s", dest.Type)
+		return nil, fmt.Errorf("unsupported log output type: %s", cfg.Destination)
 	}
 }
